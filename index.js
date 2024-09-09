0e503cd41ef50b8e6e643ef919103c61dec84785
Shade78 <shademoon322@gmail.com>
2024-09-09 04:54:36 +0300

    added validator and create user in database
    encrypt password;
    hide password from response to user;

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.1rhdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://admin:wwwwww@cluster0.1rhdw.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("DB ok");
@@ -18,31 +24,53 @@ const app = express();
 app.use(express.json()); // позволяет читать json из запросов
 
 app.get("/", (req, res) => {
-  res.send("1111");
+  res.send("main page is ok");
 });
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-  if (req.body.email === "test@test.ru") {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10); // алгоритм шифрования пароля
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
     const token = jwt.sign(
       {
-        email: req.body.email,
-        fullname: "Иван Иванов",
+        _id: user._id,
       },
-      "secret123"
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
     );
-  }
 
-  res.json({
-    success: true,
-    token,
-  });
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "не удалось зарегистрироваться",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
   if (err) {
-    return console.log(err);
+    return console.log("213", err);
   }
 
   console.log("Server OK");
